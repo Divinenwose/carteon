4f3bfc5551f666f90189e3c4e0d71f12528e2e79
Divine Nwose <nwosedivine40@gmail.com>
2026-05-02 23:02:17 +0100

    Updated

--- backend/src/routes/productRoutes.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { ProductController } from '../controllers/ProductController';
-
-export const productRouter = Router();
-const productController = new ProductController();
-
-productRouter.get('/cards', productController.getCards);
--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -51,7 +51,7 @@ const Footer = () => {
                         <ul className="mt-[10px] flex flex-col gap-4">
                             <li><a href="#feature" className="font-inter font-normal text-[14px] leading-[20px] tracking-[0px] text-[#99A1AF]">Privacy Policy</a></li>
                             <li><a href="#pricing" className="font-inter font-normal text-[14px] leading-[20px] tracking-[0px] text-[#99A1AF]">Terms of Service</a></li>
-                            <li><a href="/shipping-policy" className="font-inter font-normal text-[14px] leading-[20px] tracking-[0px] text-[#99A1AF]">Shipping policy</a></li>
+                            <li><a href="/shipping-policy" className="font-inter font-normal text-[14px] leading-[20px] tracking-[0px] text-[#99A1AF]">Shipping Policy</a></li>
                         </ul>
                     </div>
 
